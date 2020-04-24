7f04af58db5460324044016bdbce9e403d5d0776
Jose Ulloa <jose.ulloa@digitalglobe.com>
2020-04-24 14:16:01 -0600

    small fix

--- api/sockets.js
+++ api/sockets.js
@@ -78,7 +78,7 @@ module.exports = io => {
                     io.to(host._id).emit('user_data', host)
                     io.to(room._id).emit('room_data', { action: 'word_guessed', room, users })
 
-                    if(countUsersGuessed == users.length) {
+                    if(countUsersGuessed == users.length - 1) {
                         if(room.words.length == 0 || room.max_rounds - 1 <= room.round) {
                             io.to(room._id).emit('message', { user: { name: admin_name }, text: `Everyone guess the word ${room.word.name}`, color: '#ffff80' })
                             end_game(room)
